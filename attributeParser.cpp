ab57ce00a1c8ffc49ad98febcb864db7fe0f3ac2
Phannawich Jadpotwanich <phannawich1122@gmail.com>
2026-01-05 09:33:46 -0800

    redo attributeParser.cpp

--- attributeParser.cpp
+++ attributeParser.cpp
@@ -5,79 +5,71 @@
 #include <algorithm>
 #include <sstream>
 #include <optional>
+// intclude <
+#include <bits/stdc++.h>
 using namespace std;
-
-struct AttrVal {
-    string attr;
-    string val;
-};
-
-struct Record {
-    int tag;
-    vector<AttrVal> item;
-};
-
-optional<string> getValue(const vector<Record>& records, int num, const string& attr) {
-    for (const auto& r : records) {
-        if (r.tag == num) {
-            for (const auto& it : r.item) {
-                if (it.attr == attr) return it.val;
-            }
-            return nullopt; // num found, attr not found
-        }
-    }
-    return nullopt; // num not found
-}
-
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int n, q;
-    scanf("%d %d\n", &n, &q);
-    vector<Record> records (n);
-    stringstream ss;
-    string tagString;
-    string tag;
-    int tagNumber;
-    string attribute;
-    string equalSign;
-    string value;
+
+    cin >> n >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // eat the rest of the first line
+
+    // scanf("%d %d\n", &n, &q);
+    unordered_map<string, string> mp;
+    vector<string> tagStack;
+    
     for (int i = 0; i < n; i++) {
-       getline(cin, tagString);
-       if (tagString[1] == '/') {
-        continue;
+        string line;
+       getline(cin, line);
+       if (line.size() >= 2 && line[1] == '/'){
+        if(!tagStack.empty()){
+            tagStack.pop_back();
+        }
+            continue;
        }
-       tagString.erase(tagString.begin());
-       tagString.pop_back();
-    //    cout << tagString << endl;
-        ss.str(tagString);
-        ss >> tag >> attribute >> equalSign >> value;
-    //    cout << tag << endl;
-    //    cout << tag << ' '<< attribute << ' ' << equalSign << ' ' << value << endl;
-        tagNumber = int((tag[3]) - '0'); // Hold number 1 -> n
-        value.erase(value.begin());
-        value.pop_back();
-        records[tagNumber - 1].item.push_back({attribute, value});
-       ss.clear();
-    }
+       if (!line.empty() && line.front() == '<'){
+        line.erase(line.begin());
+       }
+       if (!line.empty() && line.back() == '>'){
+        line.pop_back();
+       }
+       
+       string tag;
+       stringstream ss(line);
+       ss >> tag;
+       
+       tagStack.push_back(tag);
+       
+       string path = tagStack[0];
+       for (int j = 1; j < (int)tagStack.size() ; j++) {
+        path += '.' + tagStack[j];
+       }
+       
+       string attr, eq, val;
+       while(ss >> attr >> eq >> val){
+        if(!val.empty() && val.front() == '"'){
+            val.erase(val.begin());
+        }
+        if (!val.empty() && val.back() == '"') {
+            val.pop_back();
+        }
+        mp[path + '~' + attr] = val;
+       }
+    }       
     
-    
-    
-    string queryString;
-    char c;
-    // print out
     for (int i = 0; i < q; i++) {
-        getline(cin, queryString);
-        for (int j = 0; j < queryString.size(); j++) {
-            if (queryString[j] == '~') {
-                tagNumber = queryString[j - 1] - '0';
-                queryString.erase(0, j + 1);
-                // cout << tagNumber << queryString;
-                if (auto v = getValue(records, tagNumber, queryString)) {
-                    cout << *v << "\n";  // M
-                } else {
-                    cout << "Not Found!\n";
-                }
-            }
+        string query;
+        getline(cin, query);
+        
+        auto it = mp.find(query);
+        if(it == mp.end()){
+            cout << "Not Found!\n";
+        }
+        else{
+            cout << it->second << '\n';
         }
     }
     return 0;
