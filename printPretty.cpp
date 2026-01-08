cf2b3f6c89e8056b6a6e81edb0efeb7ddb661035
Phannawich Jadpotwanich <phannawich1122@gmail.com>
2026-01-08 15:26:06 -0800

    [printPretty] implement type B

--- printPretty.cpp
+++ printPretty.cpp
@@ -8,8 +8,9 @@ using namespace std;
 
 char baseTenToSixTeen(int number){
     char result;
+    string digit = "0123456789ABCDEF";
     if (number > 9) {
-        //TODO
+        result = digit[number];
     }else{
         result = number + '0';
     }
@@ -26,19 +27,29 @@ int main() {
         cin >> A;
         string hexaA = "";
         int powerOfSixTeen = 0;
-        int coefficient = 0; 
+        int coefficient = 0;
+        int leftOver = 0;
         do {
             coefficient = (A / pow(16 ,powerOfSixTeen)); 
             if(coefficient >= 16){
                 powerOfSixTeen++;
             }else {
                 if (powerOfSixTeen == 0) {
-                    hexaA += to_string(coefficient);
+                    hexaA += baseTenToSixTeen(coefficient);
+                    // cout << A << endl;
+                    A = A - coefficient;
+                    // cout << A << endl;
                 }else {
-                    //TODO
+                    leftOver = A % int(pow(16, powerOfSixTeen));
+                    hexaA += baseTenToSixTeen(coefficient);
+                    A = leftOver;
+                    powerOfSixTeen--;
+                    if(powerOfSixTeen == 0 && leftOver == 0){
+                        hexaA += '0';
+                    }
                 }
             }
-        } while (powerOfSixTeen == 0);
+        } while (powerOfSixTeen != 0 || A != 0);
         cout << "0x" << hexaA << endl;
         // B
         double B;
