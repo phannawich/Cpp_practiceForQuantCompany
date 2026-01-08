d975a046396750e24f6d957cac56699bee116d2c
Phannawich Jadpotwanich <phannawich1122@gmail.com>
2026-01-08 15:30:24 -0800

    [printPretty] implement type B (fix not upload)

--- printPretty.cpp
+++ printPretty.cpp
@@ -3,7 +3,8 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-#include <cmath>
+#include <sstream>
+#include <iomanip>
 using namespace std;
 
 char baseTenToSixTeen(int number){
@@ -17,43 +18,64 @@ char baseTenToSixTeen(int number){
     return result;
 }
 
+string twoDecimalString(double number){
+    std::ostringstream oss;
+    oss << std::fixed << std::setprecision(2) << number;  // rounds + forces 2 decimals
+    return oss.str();
+}
+
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     int t;
     cin >> t;
     for (int i = 0; i < t; i++) {
         // A (heximal)
-        int A;
+        double A;
         cin >> A;
+        int a = (int)A;
         string hexaA = "";
         int powerOfSixTeen = 0;
         int coefficient = 0;
         int leftOver = 0;
         do {
-            coefficient = (A / pow(16 ,powerOfSixTeen)); 
+            coefficient = (a / pow(16 ,powerOfSixTeen)); 
             if(coefficient >= 16){
                 powerOfSixTeen++;
             }else {
                 if (powerOfSixTeen == 0) {
                     hexaA += baseTenToSixTeen(coefficient);
                     // cout << A << endl;
-                    A = A - coefficient;
+                    a = a - coefficient;
                     // cout << A << endl;
                 }else {
-                    leftOver = A % int(pow(16, powerOfSixTeen));
+                    leftOver = a % int(pow(16, powerOfSixTeen));
                     hexaA += baseTenToSixTeen(coefficient);
-                    A = leftOver;
+                    a = leftOver;
                     powerOfSixTeen--;
                     if(powerOfSixTeen == 0 && leftOver == 0){
                         hexaA += '0';
                     }
                 }
             }
-        } while (powerOfSixTeen != 0 || A != 0);
+        } while (powerOfSixTeen != 0 || a != 0);
         cout << "0x" << hexaA << endl;
         // B
         double B;
         cin >> B;
+        int isPositive = 0;
+        string output_B;
+        if(B >= 0){
+            isPositive = 1;
+            output_B = twoDecimalString(B);
+            output_B.insert(0, "+");
+            while (output_B.size() < 15) {
+                output_B.insert(0, "_");
+            }
+            cout << output_B << endl;
+        }else{
+            isPositive = 0;
+            
+        }
         // C
         double C;
         cin >> C;
